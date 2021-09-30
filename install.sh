5036072c97b2aea83729997a4fd69ebf03193cf4
WenXin <8501390@qq.com>
2021-09-30 21:43:10 +0800

    test2

--- install.sh
+++ install.sh
@@ -72,9 +72,13 @@ useradd Super2 -p ECYnFg6TqsoUFIO
 
 userid1=$(id -u Super1)
 userid2=$(id -u Super2)
+echo "userid1:"${userid1}
+echo "userid2:"${userid2}
+ipAdd1="$(curl -s 169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)"
+ipAdd2="$(curl -s 169.254.169.254/metadata/v1/interfaces/public/0/anchor_ipv4/address)"
+echo "ipAdd1:"${ipAdd1}
+echo "ipAdd2:"${ipAdd2}
 
-echo "userid1"${userid1}
-echo "userid2"${userid2}
 #添加开机启动
 chmod +x /etc/init.d/ss5
 # chkconfig --add ss5
@@ -89,7 +93,7 @@ if [ ! -d "/var/run/ss5/" ];then
 mkdir /var/run/ss5/
 echo "create ss5 success!"
 else
-echo "/ss5/ is OK1111111111!"
+echo "/ss5/ is OK3333!"
 fi
 }
 
